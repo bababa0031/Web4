ad2cf8447d1641d6eeee19bf7ecab56518eaf93c
bababa0031 <130861926+bababa0031@users.noreply.github.com>
2023-04-15 21:12:04 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var express = require("express");
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    socket.on('send message', function(msg) {
+        io.emit('receive message', msg);
+    });
+});
+
+http.listen(3000, function() {
+    console.log('listening on *:3000');
+});
+
+
